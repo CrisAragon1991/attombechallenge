62a665d28ea94f19731989abd60913996da056d6
Cristhian Aragon <cristianaragon32a@gmail.com>
2025-11-24 19:17:03 -0600

    Se elimino el tracking de la carpeta dist

--- dist/shared/id.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateId = generateId;
-function generateId() {
-    const uuid = crypto.randomUUID();
-    return uuid;
-}
