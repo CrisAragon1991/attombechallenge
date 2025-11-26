d1676363a9304c6510e75834fb08ff194c4a5768
Cristhian Aragon <cristianaragon32a@gmail.com>
2025-11-26 13:37:34 -0600

    Se modifico el listado de url validas para hacer peticiones

--- src/infra/cors.ts
+++ src/infra/cors.ts
@@ -1,14 +1,11 @@
 import cors from 'cors';
 
 export const corsOptions = {
-  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
-    if (!origin) return callback(null, true);
-    // Permitir cualquier puerto de localhost o 127.0.0.1
-    if (/^http:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/.test(origin)) {
-      return callback(null, true);
-    }
-    callback(new Error('Not allowed by CORS'));
-  },
+  origin: [
+    'http://localhost:4200',
+    'http://127.0.0.1:4200',
+    'https://atom-fe-challenge.onrender.com',
+  ],
   credentials: true,
 };
 
